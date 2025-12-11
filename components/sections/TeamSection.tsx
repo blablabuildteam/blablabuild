dff7f58c676986591ff9f3c595568e4160c29bd0
Xennith Oosterveer <hello@xkgrowth.com>
2025-12-11 14:29:34 +0100

    Update Team section background color to #e7e8ff

--- components/sections/TeamSection.tsx
+++ components/sections/TeamSection.tsx
@@ -103,7 +103,7 @@ export default function TeamSection() {
     <section 
       id="over-ons" 
       className="min-h-screen flex flex-col justify-center px-4 md:px-16 py-16 md:py-24"
-      style={{ backgroundColor: '#f5f5f5' }}
+      style={{ backgroundColor: '#e7e8ff' }}
     >
       <div className="mx-auto w-full max-w-[1312px]">
         {/* Header */}
